68a3c58286255c0045be60afeba24d4449665231
Aashiq Ramachandran <aashiq.r@cyware.com>
2021-02-08 10:49:56 +0530

    add files

---
+++ README.md
@@ -0,0 +1,86 @@
+# STIXOps - Interface to STIX
+### Table of contents
+------------
+- [Introduction](#Introduction)
+- [Usage](#Usage)
+- Elevate
+- Validate
+- Interface
+- [Contributing](#Contributing)
+- [Credits](#Credits)
+
+------------
+
+## Introduction
+Threat Intelligence analysts often struggle with receiving or sharing information that is machine readable and contextual. Machines are not able to ingest plain data which we see on the web even if it has context and the data format readable by machines is often not shared. STIX is the solution for both.
+
+Structured Threat Information Expression (STIX) is a language and serialization format used to exchange cyber threat intelligence (CTI). With STIX, all aspects of suspicion, compromise and attribution can be represented clearly with objects and descriptive relationships.
+
+  
+
+STIX information can be visually represented for an analyst or stored as JSON to be quickly machine readable. STIX's openness allows for integration into existing tools and products or utilized for your specific analyst or network needs.
+
+  
+
+This tool allows analysts to easily work with, convert and create STIX objects.
+
+  
+  
+
+## Usage
+
+To use this tool, first install the requirements using
+
+  
+
+pip3 install requirements.txt
+
+  
+
+After installing requirements launch the program by running
+
+  
+
+python3 main.py
+
+  
+
+### Elevate
+
+To elevate a STIX 1.0/1.1 object into a STIX 2.0/2.1 we can use this option. To elevate a STIX 1 XML, move the XML file into the directory with the converter. After keeping the file in the same folder, select the 1'st option and enter the filename to elevate.
+
+  
+
+This would elevate the STIX 1 XML into a STIX 2 JSON
+
+  
+
+### Validate
+
+We also have an option to validate/ verify the STIX 2 JSON object. To do this enter option 2 and enter the JSON filename to validate. This would result in an output mentioning if the JSON object is valid or not.
+
+  
+
+### Interface
+
+This tool also has an option to create a STIX object. To do this enter 3, and choose for what you would like to create the object. After choosing your option, enter the required data to get a fully created, valid and machine readable STIX 2.1 JSON object.
+
+## Contributing
+
+  
+
+We are constantly on the lookout for newer services/ tools to add to our repo. If you would like to contribute the easiest way to do so would be to open an issue and suggest the tool you would like.
+
+  
+
+You can also pull this repo, add your connector and submit a merge request for adding your code directly in our repo!
+
+  
+
+## Credits
+
+- https://oasis-open.github.io/cti-documentation/stix/intro.html
+
+------------------------------------------------------------------------------------------------------------------------------
+
+  
---
+++ main.py
@@ -0,0 +1,174 @@
+import json  # STIX to CSV converter
+import csv
+import os  # For TAXII connector
+from stix2elevator import elevate  # STIX elevator
+from stix2elevator.options import initialize_options
+from stix2validator import validate_file, print_results  # STIX validator
+from stix2slider import slide_file  # STIX slider
+from stix2slider.options import initialize_options
+from stix2 import *  # Python STIX interface
+from stix2elevator import elevate
+from stix2elevator.options import initialize_options
+
+
+class STIXOps (object):
+
+    def elevatefile(self, filename):
+        """
+        Convert a STIX 1 XML file to a STIX 2 JSON
+        :param filename:
+        """
+        initialize_options ()
+        with open (filename, 'r') as stixfile:
+            results = elevate (stixfile.read ())
+        print (results)
+
+    def validatefile(self, filename):
+        """
+        Validate a STIX 2 file ensuring its authenticity
+        :param filename:
+        """
+        results = validate_file (filename)
+        print_results (results)
+
+    def convert(self, filename):
+        """
+        Convert a STIX 2 JSON to a CSV file
+        :param filename:
+        """
+        with open (filename) as json_file:
+            data = json.load (json_file)
+            stix_data = data['objects']
+        data_file = open ('data_file.csv', 'w')
+        csv_writer = csv.writer (data_file)
+        count = 0
+        for object in stix_data:
+            if count == 0:
+                header = object.keys ()
+                csv_writer.writerow (header)
+                count += 1
+                csv_writer.writerow (object.values ())
+                data_file.close ()
+
+    def writeSTIX(self, option):
+        """
+        Create STIX 2.1 JSON files by answering few questions
+        :param option:
+        :return:
+        """
+        if option == 1:
+            name = str (input ("Enter indicator name: "))
+            pattern = str (input ("Enter pattern: "))
+            indicator = indicator = Indicator (name=name,
+                                               pattern=pattern,
+                                               pattern_type="stix")
+            return indicator
+
+        elif option == 2:
+            name = str (input ("Enter name: "))
+            description = str (input ("Enter description: "))
+            type = str (input ("Enter threat actor type: "))
+            resource = str (input ("Enter resource level: "))
+            role = str (input ("Enter role: "))
+            motivation = str (input ("Enter primary motivation: "))
+            alias = str (input ("Enter alias: "))
+            threatactor = ThreatActor (name=name,
+                                       description=description,
+                                       threat_actor_types=type,
+                                       resource_level=resource,
+                                       roles=role,
+                                       primary_motivation=motivation,
+                                       aliases=alias)
+            return threatactor
+
+        elif option == 3:
+            name = str (input ("Enter malware name: "))
+            family = str (input ("Does the malware have any relations [True/ False]? "))
+            description = str (input ("Enter malware description: "))
+            type = str (input ("Enter malware type: "))
+            malware = Malware (name=name,
+                               description=description,
+                               is_family=family,
+                               malware_types=type)
+            return malware
+
+        elif option == 4:
+            name = str (input ("Enter identity name: "))
+            id_class = str (input ("Enter identity class: "))
+            sector = str (input ("Enter sector: "))
+            contact = str (input ("Enter contacts: "))
+            identity = Identity (name=name,
+                                 identity_class=id_class,
+                                 sectors=sector,
+                                 contact_information=contact)
+            return identity
+
+        elif option == 5:
+            name = str (input ("Enter tool name: "))
+            type = str (input ("Enter tool type: "))
+            description = str (input ("Enter description: "))
+            phases = str (input ("Enter tool phases: "))
+            tool = Tool (name=name,
+                         tool_types=type,
+                         description=description)
+            return tool
+
+        elif option == 6:
+            name = str (input ("Enter attack pattern name: "))
+            description = str (input ("Enter attack pattern description: "))
+            attackpattern = AttackPattern (name=name,
+                                           description=description,
+                                           )
+            return attackpattern
+
+        elif option == 7:
+            name = str (input ("Enter report name: "))
+            type = str (input ("Enter report type: "))
+            description = str (input ("Enter report description: "))
+            ref = str (input ("Enter object references: "))
+            report = Report (name=name,
+                             report_types=type,
+                             description=description,
+                             object_refs=ref)
+            return report
+
+        elif option == 8:
+            name = str (input ("Enter campaign name: "))
+            description = str (input ("Enter campaign description: "))
+            objective = str (input ("Enter objective: "))
+            campaign = Campaign (name=name,
+                                 description=description,
+                                 objective=objective)
+            return campaign
+
+        elif option == 9:
+            name = str (input ('Enter vulnerability name: '))
+            description = str (input ("Enter vulnerability description: "))
+            labels = str (input ("Enter labels: "))
+            vulnerability = Vulnerability (name=name,
+                                           description=description,
+                                           labels=labels)
+            return vulnerability
+
+    def main(self):
+        option = int (input (
+            "[+] Welcome to STIX ops. Enter task to perform\n [-][1] For "
+            "elevating STIX 1 to STIX 2\n [-][2] For validating a STIX json file\n [-][3] For creating a STIX object "
+            "via an interface\n --> "))
+        if option == 1:
+            filename = str (input ("[+] Enter filename to elevate [File must be in this directory]: "))
+            self.elevatefile (filename)
+        elif option == 2:
+            filename = str (input ("[+] Enter filename to validate [File must be in this directory]: "))
+            self.validatefile (filename)
+        elif option == 3:
+            option = int (input (
+                "Enter option to parse\n [-][1] For indicator\n [-][2] For threat actor\n [-][3] For malware\n [-][4] "
+                "For identity\n [-][5] For tool\n [-][6] For attack pattern\n [-][7] For report\n [-][8] For "
+                "campaign\n [-][9] For vulnerability\n --> "))
+            result = self.writeSTIX (option)
+            print (result)
+
+
+x = STIXOps ()
+x.main ()
